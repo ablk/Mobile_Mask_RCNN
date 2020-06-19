4f6012e44ceeb8be00487a40ccae73283b288c95
ablk <31957211+ablk@users.noreply.github.com>
2020-06-19 16:07:29 +0800

    Add files via upload

--- collection/run_all.sh
+++ collection/run_all.sh
@@ -4,5 +4,13 @@
 ./move.sh
 python modifyxml.py
 rm scene_all/scene_000002000002.xml
-python voc2coco/xml2coco.py scene_all/ out_coco --labels sis_labels.txt 
+mkdir scene_all/val
+mv scene_all/*0.jpg scene_all/val
+mv scene_all/*0.xml scene_all/val
 
+mkdir scene_all/train
+mv scene_all/*.jpg scene_all/train
+mv scene_all/*.xml scene_all/train
+
+python voc2coco/xml2coco.py scene_all/val out_coco_val --labels sis_labels.txt
+python voc2coco/xml2coco.py scene_all/train out_coco_train --labels sis_labels.txt
